6c9670c4fd17e1578caff6877fb99ce77c8ccb03
Frank <frank@iMac3.local>
2018-12-19 10:08:47 -0500

    working on the paddle

--- breakout.py
+++ breakout.py
@@ -1,5 +1,7 @@
 import pygame, sys
 from pygame.locals import *
+import brick
+import paddle
 
 def main():
     # Constants that will be used in the program
@@ -24,26 +26,31 @@ def main():
     CYAN = (0, 255, 255)
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
+    color = [RED, ORANGE, YELLOW, GREEN, CYAN]
+    mainSurface = pygame.display.set_mode((400, 600), 32, 0)
 
     # Step 1: Use loops to draw the rows of bricks. The top row of bricks should be 70 pixels away from the top of
     # the screen (BRICK_Y_OFFSET)
-    pygame.init()
-    main_surface = pygame.display.set_mode((400, 600), 32, 0)
-    pygame.display.set_caption("Bricks")
-    width =
-    bricks = brick.Brick(mainSurface, )
-    bricks.draw()
+    x = 0
+    y = BRICK_Y_OFFSET
+    for a in color:
+        for b in range(2):
+            for c in range(BRICKS_PER_ROW):
+                my_brick = brick.Brick(BRICK_WIDTH, BRICK_HEIGHT, a)
+                my_brick.rect.x = x
+                my_brick.rect.y = y
+                mainSurface.blit(my_brick.image, my_brick.rect)
+                x = x +BRICK_WIDTH + BRICK_SEP
+            y = y +BRICK_HEIGHT + BRICK_SEP
+            x = 0
+    mainSurface.fill(WHITE)
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+        pygame.display.update()
 
-    while True:
-        for event in pygame.event.get():
-            if event == QUIT:
-                pygame.quit()
-                sys.exit()
 
 main()
